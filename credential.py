962e48f2b97b81322be8805ab1eb88b7f6baad18
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-06-28 23:03:46 +0300

    create class and method for defining properties of objects

---
+++ credential.py
@@ -0,0 +1,14 @@
+class Card:
+    '''
+    class for creating new intance
+
+    '''
+
+    def __init__(self, username, accounts, email):
+        '''
+        __init__method helps define properties for our objects.
+
+        '''
+        self.username = username
+        self.accounts = accounts
+        self.email = email
